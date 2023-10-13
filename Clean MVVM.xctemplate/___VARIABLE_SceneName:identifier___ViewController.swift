ccf6565ca7386cd29baa5b14f803d2a1a81952f3
Bilal Durnagöl <bilaldurnagol@gmail.com>
2023-10-13 20:15:09 +0300

    done

---
+++ Clean MVVM.xctemplate/___VARIABLE_SceneName:identifier___ViewController.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_SceneName___ViewController: UIViewController {
+    
+    var viewModel: ___VARIABLE_SceneName___ViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        precondition(viewModel != nil, "viewModel can not be nil!")
+    }
+}
---
+++ Clean MVVM.xctemplate/___VARIABLE_SceneName:identifier___ViewModel.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import Foundation
+
+// MARK: - View model inputs
+protocol ___VARIABLE_SceneName___ViewModelInput {
+    
+}
+
+// MARK: - View model outputs
+protocol ___VARIABLE_SceneName___ViewModelOutput {
+    
+}
+
+/// Alias for view model input and output
+typealias ___VARIABLE_SceneName___ViewModel = ___VARIABLE_SceneName___ViewModelInput & ___VARIABLE_SceneName___ViewModelOutput
+
+/// Default___VARIABLE_SceneName___ViewModel implementation
+final class Default___VARIABLE_SceneName___ViewModel: ___VARIABLE_SceneName___ViewModel {
+    
+}
